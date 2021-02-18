bd6cbe9a9aab28894ed847c162fb3075dc1624a0
Hermes Passer <dsldouglas@hotmail.com>
2021-02-18 15:30:25 -0300

    move ITileset to its own file and add ToString methods

---
+++ Project/Code/ITileset.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS1591
+namespace tilecon
+{
+    public interface ITileset
+    {
+        /// <summary>Get the tileset name.</summary>
+        /// <returns>Tileset name.</returns>
+        string TilesetName();
+
+        /// <summary>Get the sprite size.</summary>
+        /// <returns>Sprite size.</returns>
+        int SpriteSize();
+
+        /// <summary>Get the tileset width.</summary>
+        /// <returns>Tileset width.</returns>
+        int SizeWidth();
+
+        /// <summary>Get the tileset height.</summary>
+        /// <returns>Tileset height.</returns>
+        int SizeHeight();
+
+        //TODO: add when move to cs8
+        //string Name { get; set; }
+        //string ToString() => Name;
+    }   
+}
